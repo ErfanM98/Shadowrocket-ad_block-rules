a57493525421a3c5a7edf84a0c5e92da59304671
Erfan Moharramkhani <39065975+ErfanM98@users.noreply.github.com>
2023-06-02 11:40:47 +0330

    Update rj_req.js

--- radiojava/rj_req.js
+++ radiojava/rj_req.js
@@ -1,1 +1,13 @@
+service = $request.url.split("/").slice(-1)[0].split("?")[0];
 
+if (!(service == "login")){
+    if (!"headers" in $request) {
+        $request.headers = {};
+    }
+    if (!"Cookie" in $request.headers) {
+        $request.headers.Cookie = $persistentStore.read("RadioJavanCoockie");
+    }else {
+        $request.headers.Cookie = $persistentStore.read("RadioJavanCoockie") + "; " + $request.headers.Cookie;
+    }
+}
+$done($request);
