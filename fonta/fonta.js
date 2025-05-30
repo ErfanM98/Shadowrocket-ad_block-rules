a4d731c5acdf4d7b80b240c2b1880cc7c86eb321
Erfan Moharramkhani <39065975+ErfanM98@users.noreply.github.com>
2025-05-30 12:59:19 +0000

    Update fonta.js

--- fonta/fonta.js
+++ fonta/fonta.js
@@ -1,4 +1,8 @@
 var data = JSON.parse($response.body);
-data.fontaCustomAdService.ads = [];
+//data.fontaCustomAdService.ads = [];
+data.fontaCustomAdService.ads[0].skipAvailableAt = '0:00:00.000000';
+data.fontaCustomAdService.frequencyCap = 1000;
+data.adsFrequencyCap = 1000;
+
 $response.body = JSON.stringify(data);
 $done($response);
